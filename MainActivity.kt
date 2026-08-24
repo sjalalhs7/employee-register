0729b59394d3d364253edf8d70bf141a9a9602c8
sjalalhs7 <sjalalhs7@gmail.com>
2026-08-24 15:08:52 +0500

    Add files via upload

--- MainActivity.kt
+++ MainActivity.kt
@@ -3,30 +3,28 @@ package com.msm.securityguards
 import android.annotation.SuppressLint
 import android.app.Activity
 import android.os.Bundle
-import android.webkit.CookieManager
+import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
-import android.webkit.WebChromeClient
-import android.content.Intent
-import android.net.Uri
 
 class MainActivity : Activity() {
+    private lateinit var webView: WebView
+
     @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val web = findViewById<WebView>(R.id.webview)
-        web.settings.javaScriptEnabled = true
-        web.settings.domStorageEnabled = true
-        web.settings.allowFileAccess = false
-        web.webViewClient = object : WebViewClient() {
-            override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
-                if (url.startsWith("http://") || url.startsWith("https://")) return false
-                return try { startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(url))); true } catch (_: Exception) { true }
-            }
-        }
-        web.webChromeClient = WebChromeClient()
-        CookieManager.getInstance().setAcceptCookie(true)
-        web.loadUrl("https://sjalalhs7.github.io/employee-register/")
+        webView = WebView(this)
+        webView.settings.javaScriptEnabled = true
+        webView.settings.domStorageEnabled = true
+        webView.settings.allowFileAccess = false
+        webView.settings.allowContentAccess = true
+        webView.webViewClient = WebViewClient()
+        webView.webChromeClient = WebChromeClient()
+        webView.loadUrl("https://sjalalhs7.github.io/employee-register/")
+        setContentView(webView)
+    }
+
+    override fun onBackPressed() {
+        if (webView.canGoBack()) webView.goBack() else super.onBackPressed()
     }
 }
