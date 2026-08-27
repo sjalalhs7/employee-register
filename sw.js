61a0b7fa2fe5f94a2cadd7ee6b90d3f14405d19b
sjalalhs7 <sjalalhs7@gmail.com>
2026-08-27 09:21:38 +0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,20 @@
-const CACHE='msm-longroll-v2';
-const APP_SHELL=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./icon-512-maskable.png','./msm-security-guards-logo.png'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(APP_SHELL)).then(()=>self.skipWaiting()))});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return; e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))))});
+const CACHE = 'msm-longroll-v1';
+const APP_SHELL = [
+  './', './index.html', './manifest.webmanifest', './msm-security-guards-logo.png',
+  './icons/msm-icon-192.png', './icons/msm-icon-512.png'
+];
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(APP_SHELL)).then(()=>self.skipWaiting()));
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(()=>self.clients.claim()));
+});
+self.addEventListener('fetch', event => {
+  const req = event.request;
+  if (req.method !== 'GET') return;
+  event.respondWith(fetch(req).then(res => {
+    const copy = res.clone();
+    caches.open(CACHE).then(cache => cache.put(req, copy)).catch(()=>{});
+    return res;
+  }).catch(() => caches.match(req).then(r => r || caches.match('./index.html'))));
+});
